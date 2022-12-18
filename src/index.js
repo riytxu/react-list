aaecfd7d087010aa022eace465e7916fd290d123
Aleksey Balahnin <alesha961337@yandex.ru>
2022-12-18 18:49:28 +1000

    fix redux error

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 
 import App from "./App";
 
-import { store } from "./app/store";
+import { store } from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
