dd26792539ccc64e09d1f90c6088a77515efd9d9
Aleksey Balahnin <alesha961337@yandex.ru>
2023-01-13 20:40:58 +1000

    add .env file and move route provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { MainPage } from "./components/MainPage/MainPage";
 
 import "./App.css";
 
 function App() {
-  return <MainPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
